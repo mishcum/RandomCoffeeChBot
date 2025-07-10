d9036913db8d12d44404863f059cae9bc36c586c
Mikhail <m6453327@icloud.com>
2025-07-10 07:31:28 +0300

    registration added

--- app/bd.py
+++ app/bd.py
@@ -1,9 +1,20 @@
-from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.ext.asyncio import AsyncAttrs, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy import Integer, BigInteger, Text, DateTime, CheckConstraint
+from sqlalchemy import Integer, BigInteger, Text, DateTime, CheckConstraint, ForeignKey
 from datetime import datetime
 from sqlalchemy.sql import func
 
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+DB_URL = os.getenv("DB_URL")
+
+if not DB_URL:
+    raise ValueError('DB_URL is not set in the environment variables.')
+
+engine = create_async_engine(DB_URL, echo=False, pool_size=10, max_overflow=20)
+Session = async_sessionmaker(engine, expire_on_commit=False)
 
 class Base(AsyncAttrs, DeclarativeBase):
     pass
@@ -18,19 +29,22 @@ class User(Base):
     last_name: Mapped[str] = mapped_column(Text)
     registered: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now())
 
-    meetings_as_a: Mapped[list['Meeting']] = relationship(back_populates='user_a', cascade='all, delete-orphan', foreign_keys='Meeteing.user_a_id')
-    meetings_as_b: Mapped[list['Meeting']] = relationship(back_populates='user_b', cascade='all, delete-orphan', foreign_keys='Meeteing.user_b_id')
+    meetings_as_a: Mapped[list['Meeting']] = relationship(back_populates='user_a', cascade='all, delete-orphan', foreign_keys='Meeting.user_a_id')
+    meetings_as_b: Mapped[list['Meeting']] = relationship(back_populates='user_b', cascade='all, delete-orphan', foreign_keys='Meeting.user_b_id')
 
 class Meeting(Base):
     __tablename__ = 'meetings'
 
-    __mapper_args__ = (CheckConstraint('user_a_id != user_b_id', name='check_diff_users'))
+    __table_args__ = (CheckConstraint('user_a_id <> user_b_id', name='check_diff_users'),)
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    user_a_id: Mapped[int] = mapped_column(Integer, nullable=False)
-    user_b_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    user_a_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
+    user_b_id: Mapped[int] = mapped_column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), default=func.now())
 
     user_a : Mapped['User'] = relationship(back_populates='meetings_as_a', foreign_keys=[user_a_id])
     user_b : Mapped['User'] = relationship(back_populates='meetings_as_b', foreign_keys=[user_b_id])
     
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
--- app/main.py
+++ app/main.py
@@ -5,8 +5,14 @@ import logging
 
 from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart
-from aiogram.types import Message
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.fsm.state import StatesGroup, State
+from aiogram.fsm.context import FSMContext
+
+from app.bd import init_db, Session, User, Meeting
+from sqlalchemy import select
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s  %(levelname)-8s %(name)s | %(message)s")
 
 load_dotenv()
 
@@ -14,9 +20,11 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 if not BOT_TOKEN:
     raise ValueError('BOT_TOKEN is not set in the environment variables.')
 
-
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
+
+class Reg(StatesGroup):
+    waiting_registration = State()
 
 regbutton = ReplyKeyboardMarkup(
     keyboard=[[KeyboardButton(text='Регистрация 🚀')]],
@@ -24,10 +32,59 @@ regbutton = ReplyKeyboardMarkup(
 )
 
 @dp.message(CommandStart())
-async def start_command(message: Message) -> None:
-    await message.answer(text=f'Привет, *{message.from_user.full_name}*!👋\nЯ бот для случайных встреч. Напиши /meet, чтобы начать.', parse_mode='Markdown')
+async def start_command(message: Message, state: FSMContext) -> None:
+    await state.clear()
+
+    async with Session() as session:
+        user = await session.scalar(select(User).where(User.tg_id == message.from_user.id))
+
+    if user:
+        await message.answer(text=f'С возвращением, *{message.from_user.full_name}*!👋\nНапиши /meet, чтобы начать.', parse_mode='Markdown')
+    else:
+        await message.answer(text=f'Привет, *{message.from_user.full_name}*!👋\nЯ бот для случайных встреч. Зарегестрируйся, чтобы начать! ☕️', parse_mode='Markdown', reply_markup=regbutton)
+
+@dp.message(F.text == 'Регистрация 🚀')
+async def registration(message: Message, state : FSMContext) -> None:
+    async with Session() as session:
+        is_exists = await session.scalar(select(User).where(User.tg_id == message.from_user.id))
+        if is_exists:
+            await message.answer(text='Ты уже зарегестрирван!✅\nНапиши /meet, чтобы начать.', parse_mode='Markdown', reply_markup=ReplyKeyboardRemove())
+            return
+        else:
+            await message.answer(text='Давай знакомиться! 😀\nНапиши свои *имя и фамилию* одним сообщением, например: `Лена Головач`', parse_mode='Markdown', reply_markup=ReplyKeyboardRemove())
+            await state.set_state(Reg.waiting_registration)
+
+@dp.message(Reg.waiting_registration, F.text)
+async def save_name(message : Message, state : FSMContext) -> None: 
+    splitted = message.text.strip().split()
+    if len(splitted) != 2:
+        await message.answer(text='Пожалуйста, введи *имя и фамилию* одним сообщением, например: `Лена Головач`', parse_mode='Markdown')
+        return
+    
+    fname, lname = splitted
+    async with Session.begin() as session:
+        user = await session.scalar(select(User).where(User.tg_id == message.from_user.id).with_for_update())
+        if user:
+            await message.answer(text='Ты уже зарегестрирван!✅\nНапиши /meet, чтобы начать.', parse_mode='Markdown', reply_markup=ReplyKeyboardRemove())
+            await state.clear()
+            return
+        
+        session.add(User(
+            tg_id=message.from_user.id,
+            username=message.from_user.username,
+            first_name=fname,
+            last_name=lname
+        ))
+
+        await message.answer(f"Спасибо, *{fname}*! Ты c нами ✅", reply_markup=ReplyKeyboardRemove(), parse_mode='Markdown')
+        await state.clear()
+
+@dp.message(F.text)
+async def fallback(msg: Message):
+    await msg.answer("Набери /start или нажми «Регистрация» 🙂")
 
 async def main() -> None:
+    await init_db()
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
